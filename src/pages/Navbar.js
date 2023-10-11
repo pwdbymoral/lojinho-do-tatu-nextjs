a6e20e4edc4b5db37a508617ed8b8811989003b1
Vinícius Varjão <moraesvxp@gmail.com>
2023-10-11 20:38:17 -0300

    responsiveness updates

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,23 +1,30 @@
 import Image from 'next/image'
 import Logo from '../../assets/img/okay emote.webp'
+import { useState } from "react";
 
-export default function() {
-  return(
-    <div className='bg-green-400 px-4'>
-      <div className='container mx-auto flex flex-row items-center justify-between'>
-        <div className='flex flex-row items-center space-x-4'>
-          <Image src={Logo} alt='Rammus Logo'className='h-28 w-auto'/>
-          <h1 className='text-lime-900 text-3xl font-["MontSerrat"]'>
-            Lojinho do Tatu
-          </h1>
+
+
+export default function () {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    return (
+        <div className='bg-green-500'>
+            <div className='container px-4 mx-auto flex flex-row items-center justify-between'>
+                <div className='flex flex-row items-center space-x-4'>
+                    <Image src={Logo} alt='Rammus Logo' className='h-28 w-auto' />
+                    <h1 hidden className='text-lime-900 text-3xl font-["MontSerrat"]'>
+                        Lojinho do Tatu
+                    </h1>
+                </div>
+
+                <nav className='space-x-4 justify-end'>
+                    <a className='text-lg text-lime-900 hover:text-gray-900' href='#' aria-current='page'>Home</a>
+                    <a className='text-lg text-lime-900 hover:text-gray-900' href='#'>Produtos</a>
+                    <a className='text-lg text-lime-900 hover:text-gray-900' href='#'>Contato</a>
+                </nav>
+            </div>
+            <div className='container mx-auto px-8 pb-4'>
+                <input className='w-full px-4 py-2 rounded-lg' placeholder='O que você está buscando?'></input>
+            </div>
         </div>
-        
-        <nav className='space-x-4 justify-end'>
-          <a className='text-lg text-lime-900 hover:text-green-600' href='#'>Home</a>
-          <a className='text-lg text-lime-900 hover:text-green-600' href='#'>Produtos</a>
-          <a className='text-lg text-lime-900 hover:text-green-600' href='#'>Contato</a>
-        </nav>
-      </div>
-    </div>
-  )
+    )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import Image from 'next/image'
 import Navbar from './Navbar'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import ProductCard from './ProductCard'
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-    </main>
-  )
+    return (
+        <main>
+            <Navbar />
+            <div className='container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto'>
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
+        </main>
+    )
 }
