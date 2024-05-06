cba27d7ae41f7c715babf5f5c8fbb41288226c8f
Vinícius Varjão <moraesvxp@gmail.com>
2024-05-06 16:31:33 -0300

    feat: Refactor header component to extract "nav" into separate Nav component extract the "nav" element from the header component and create a separate Nav component for better organization and maintainability.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Logo from '../../../assets/img/okay emote.webp';
-import Link from 'next/link';
+import Nav from '../Nav';
 
 /**
  * Header component display website logo, a  link list to all website pages
@@ -13,40 +13,16 @@ import Link from 'next/link';
 const Header: React.FC = () => {
   return (
     <div className="bg-primary">
-      <div className="container mx-auto flex flex-col items-center justify-center md:flex-row md:justify-between md:px-4">
-        <div className="mb-2 flex w-full flex-col items-center space-x-4 md:flex-row">
-          <Image src={Logo} alt="Rammus Logo" className="h-28 w-auto" />
-          <h1 className="font-title text-3xl font-medium text-gray-100">
-            Lojinho do Tatu
-          </h1>
-        </div>
-
-        <nav className="flex w-full justify-around divide-x-[1px] divide-gray-200 bg-green-700 md:block md:w-auto md:justify-between md:space-x-4 md:space-y-0 md:divide-x-0 md:bg-inherit">
-          <Link
-            href="/"
-            className=" w-full py-2 text-center text-lg text-gray-100 hover:text-gray-900 md:inline"
-          >
-            Home
-          </Link>
-          <Link
-            href="/produtos"
-            className="w-full py-2 text-center text-lg text-gray-100 hover:text-gray-900"
-          >
-            Produtos
-          </Link>
-          <Link
-            href="/contato"
-            className="w-full py-2 text-center text-lg text-gray-100 hover:text-gray-900"
-          >
-            Contato
-          </Link>
-        </nav>
+      <div className="container mx-auto flex w-full flex-wrap items-center justify-between px-8">
+        <Image src={Logo} alt="Rammus Logo" className="h-28 w-auto" />
+        <Nav />
       </div>
+
       <div className="container mx-auto mt-4 px-8 pb-4">
         <input
           className="w-full rounded-lg px-4 py-2"
           placeholder="O que você está buscando?"
-        ></input>
+        />
       </div>
     </div>
   );
---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { Menu as MenuIcon, X as MenuClose } from 'lucide-react';
+
+const NavLinks: React.FC = () => {
+  return (
+    <>
+      <Link
+        href="/"
+        className=" w-full py-2 text-center text-lg text-gray-100 hover:text-gray-900 md:inline"
+      >
+        Home
+      </Link>
+      <Link
+        href="/produtos"
+        className="w-full py-2 text-center text-lg text-gray-100 hover:text-gray-900"
+      >
+        Produtos
+      </Link>
+      <Link
+        href="/contato"
+        className="w-full py-2 text-center text-lg text-gray-100 hover:text-gray-900"
+      >
+        Contato
+      </Link>
+    </>
+  );
+};
+
+const Nav: React.FC = () => {
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
+  const toggleNavbar = (): void => {
+    setMobileMenuIsOpen(!mobileMenuIsOpen);
+  };
+  return (
+    <>
+      <nav className="flex w-1/3 justify-end">
+        <div className="hidden w-full justify-between md:flex">
+          <NavLinks />
+        </div>
+        <div className="flex md:hidden">
+          <button onClick={toggleNavbar} className="text-gray-100">
+            {mobileMenuIsOpen ? (
+              <MenuClose size={28} />
+            ) : (
+              <MenuIcon size={28} />
+            )}
+          </button>
+        </div>
+      </nav>
+      {mobileMenuIsOpen && (
+        <div className="flex basis-full flex-col">
+          <NavLinks />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Nav;
