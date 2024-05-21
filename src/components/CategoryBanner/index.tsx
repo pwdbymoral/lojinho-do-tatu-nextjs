ad27f1a375bfe1941e402922e5cd8a0901a8e390
Vinícius Varjão <moraesvxp@gmail.com>
2024-05-21 09:09:42 -0300

    refactor: improve code readability of CategoryBanner

--- src/components/CategoryBanner/index.tsx
+++ src/components/CategoryBanner/index.tsx
@@ -9,30 +9,25 @@ interface CategoryBannerProps {
 
 /**
  * Renders a category banner component with background image and text overlay.
- *
- * The component receives two arguments: `name` and `image`.
- * - `name`: A string representing the name of the category.
- * - `image`: A string representing the URL of the category banner image.
  */
-const CategoryBanner: React.FC<CategoryBannerProps> = ({ data }) => {
-  const { name, url } = data;
+const CategoryBanner: React.FC<CategoryBannerProps> = ({
+  data: { name, url },
+}) => {
   return (
-    <div className="relative h-96 w-full md:h-[660px]">
+    <div className="relative h-[550px] w-full">
       <Image
         src={url}
         alt={name}
-        layout="fill"
+        fill={true}
         priority={true}
         className="object-cover"
       />
       <div className="absolute left-4 top-4">
-        {/* <div className=" bg-neutral-black md:bg-inherit"> */}
         <p className="text-xl font-semibold text-primary">{name}</p>
         <Link href="/produtos" className="font-light text-primary underline">
           Conferir
         </Link>
       </div>
-      {/* </div> */}
     </div>
   );
 };
